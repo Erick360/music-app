a1b2915dd6157c7daca1707f60527f5d8e3254af
eri <eri360roperez@gmail.com>
2024-10-30 20:20:56 -0600

    Updating ui

--- lib/main.dart
+++ lib/main.dart
@@ -11,121 +11,184 @@ class music_app extends StatefulWidget {
 }
 
 class _music_app extends State<music_app> {
-  void playSong(int soundNumber) async{
+  void playSong(int soundNumber) async {
     final player = AudioPlayer();
     await player.play(AssetSource('note$soundNumber.wav'));
   }
 
+  /* function that return an Expanded object
+Expanded buildKey(Color color, int soundNumber){
+    return Expanded(
+      child: TextButton(
+        onPressed: (){
+          playSong(1);
+        },
+        style: ButtonStyle(
+          shape: WidgetStateProperty.all(
+              const RoundedRectangleBorder(
+                borderRadius: BorderRadius.zero,
+              )
+          ),
+          backgroundColor:
+          WidgetStateProperty.all<Color>(const Color(0xFFF44336)),
+        ),
+        child: const Text(
+          'click',
+          style: TextStyle(
+            color: Colors.transparent,
+          ),
+        ),
+      ),
+    )
+}
+*/
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              TextButton(
-                onPressed: (){
-                 playSong(1);
-                },
-                style: ButtonStyle(
-                  backgroundColor:
-                      WidgetStateProperty.all<Color>(const Color(0xFFF44336)),
-                ),
-                child: const Text(
-                  'click',
-                  style: TextStyle(
-                    color: Colors.transparent,
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    playSong(1);
+                  },
+                  style: ButtonStyle(
+                    shape: WidgetStateProperty.all(const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero,
+                    )),
+                    backgroundColor:
+                        WidgetStateProperty.all<Color>(const Color(0xFFF44336)),
+                  ),
+                  child: const Text(
+                    'click',
+                    style: TextStyle(
+                      color: Colors.transparent,
+                    ),
                   ),
                 ),
               ),
-              TextButton(
-                onPressed: (){
-                  playSong(2);
-                },
-                style: ButtonStyle(
-                  backgroundColor:
-                      WidgetStateProperty.all<Color>(const Color(0xFFFF5722)),
-                ),
-                child: const Text(
-                  'click',
-                  style: TextStyle(
-                    color: Colors.transparent,
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    playSong(2);
+                  },
+                  style: ButtonStyle(
+                    shape: WidgetStateProperty.all(const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero,
+                    )),
+                    backgroundColor:
+                        WidgetStateProperty.all<Color>(const Color(0xFFFF5722)),
+                  ),
+                  child: const Text(
+                    'click',
+                    style: TextStyle(
+                      color: Colors.transparent,
+                    ),
                   ),
                 ),
               ),
-              TextButton(
-                onPressed: (){
-                  playSong(3);
-                },
-                style: ButtonStyle(
-                  backgroundColor:
-                      WidgetStateProperty.all<Color>(const Color(0xFFFFEB3B)),
-                ),
-                child: const Text(
-                  'click',
-                  style: TextStyle(
-                    color: Colors.transparent,
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    playSong(3);
+                  },
+                  style: ButtonStyle(
+                    shape: WidgetStateProperty.all(const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero,
+                    )),
+                    backgroundColor:
+                        WidgetStateProperty.all<Color>(const Color(0xFFFFEB3B)),
+                  ),
+                  child: const Text(
+                    'click',
+                    style: TextStyle(
+                      color: Colors.transparent,
+                    ),
                   ),
                 ),
               ),
-              TextButton(
-                onPressed: () {
-                  playSong(4);
-                },
-                style: ButtonStyle(
-                  backgroundColor:
-                      WidgetStateProperty.all<Color>(const Color(0xFF4CAF50)),
-                ),
-                child: const Text(
-                  'click',
-                  style: TextStyle(
-                    color: Colors.transparent,
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    playSong(4);
+                  },
+                  style: ButtonStyle(
+                    shape: WidgetStateProperty.all(const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero,
+                    )),
+                    backgroundColor:
+                        WidgetStateProperty.all<Color>(const Color(0xFF4CAF50)),
+                  ),
+                  child: const Text(
+                    'click',
+                    style: TextStyle(
+                      color: Colors.transparent,
+                    ),
                   ),
                 ),
               ),
-              TextButton(
-                onPressed: () {
-                  playSong(5);
-                },
-                style: ButtonStyle(
-                  backgroundColor:
-                      WidgetStateProperty.all<Color>(const Color(0xFF009688)),
-                ),
-                child: const Text(
-                  'click',
-                  style: TextStyle(
-                    color: Colors.transparent,
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    playSong(5);
+                  },
+                  style: ButtonStyle(
+                    shape: WidgetStateProperty.all(const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero,
+                    )),
+                    backgroundColor:
+                        WidgetStateProperty.all<Color>(const Color(0xFF009688)),
+                  ),
+                  child: const Text(
+                    'click',
+                    style: TextStyle(
+                      color: Colors.transparent,
+                    ),
                   ),
                 ),
               ),
-              TextButton(
-                onPressed: () {
-                  playSong(6);
-                },
-                style: ButtonStyle(
-                  backgroundColor:
-                      WidgetStateProperty.all<Color>(const Color(0xFF2196F3)),
-                ),
-                child: const Text(
-                  'click',
-                  style: TextStyle(
-                    color: Colors.transparent,
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    playSong(6);
+                  },
+                  style: ButtonStyle(
+                    shape: WidgetStateProperty.all(const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero,
+                    )),
+                    backgroundColor:
+                        WidgetStateProperty.all<Color>(const Color(0xFF2196F3)),
+                  ),
+                  child: const Text(
+                    'click',
+                    style: TextStyle(
+                      color: Colors.transparent,
+                    ),
                   ),
                 ),
               ),
-              TextButton(
-                onPressed: (){
-                  playSong(7);
-                },
-                style: ButtonStyle(
-                  backgroundColor:
-                      WidgetStateProperty.all<Color>(const Color(0xFF9C27B0)),
-                ),
-                child: const Text(
-                  'click',
-                  style: TextStyle(
-                    color: Colors.transparent,
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    playSong(7);
+                  },
+                  style: ButtonStyle(
+                    shape: WidgetStateProperty.all(const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero,
+                    )),
+                    backgroundColor:
+                        WidgetStateProperty.all<Color>(const Color(0xFF9C27B0)),
+                  ),
+                  child: const Text(
+                    'click',
+                    style: TextStyle(
+                      color: Colors.transparent,
+                    ),
                   ),
                 ),
               ),
